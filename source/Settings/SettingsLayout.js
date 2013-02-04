b5fa16f975d84f7bbb36bfd5edcec8287f16df40
Philippe <psamlong@laposte.net>
2013-02-04 17:01:54 +0100

    set title

--- source/Settings/SettingsLayout.js
+++ source/Settings/SettingsLayout.js
@@ -6,13 +6,27 @@ enyo.kind({
     },
     components: [
         { kind: "onyx.Toolbar", content: "Settings" },
-        { content: "TODO", fit: true },
+        { kind: "enyo.Scroller", fit: true, components: [
+            {
+                kind: "onyx.Groupbox",
+                components: [
+    
+                    { kind: "onyx.GroupboxHeader", content: "CouchDB database URL" },
+                    {
+                        kind: "onyx.InputDecorator",
+                        components: [
+                            { kind: "onyx.Input", placeholder: "URL" }
+                        ]
+                    }
+                ]
+            }
+        ]},
         {
             kind: "onyx.Toolbar",
             components: [
                 { kind: "onyx.Button", content: "Save", ontap: "saveTap" }
-            ]
-        }
+            ],
+        },
     ],
     saveTap: function (inSender, inEvent) {
         this.doShowDocumentPanel();
